d8d47456d9390f7c3db16287935735d80c6a244c
Nick Kroeger <kroegern1@live.com>
2022-01-21 17:36:12 -0800

    added some prints for html

--- Calculator/main.py
+++ Calculator/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import jinja2
 from IPython.display import display
 
+# TODO: sorting ignoring string case: https://stackoverflow.com/questions/10269701/case-insensitive-list-sorting-without-lowercasing-the-result
+
 import pickle
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
@@ -12,21 +14,23 @@ calculateScores(1)
 calculateScores(2)
 # printScoreboard()
 
+for p in range(0, len(player_list)):
+    print("<h2 id=\"" + player_list[p].get_username() + "\" class=\"player-name\">" + player_list[p].get_username() + "</h2>")
+    df = player_list[p].get_history(False).reset_index()
+    df_styled = df.style
+    df_styled = df_styled.hide_index().hide_columns('index').hide_columns('question_type')  # remove index columns
+    print(df_styled.hide_index().render())
+    print('\n\n')
 
+scoreboard = getScoreboard()
 
-
-
-
-
-
-
-
-# df = player_list[1].get_history(False).reset_index()
+# for player in sorted(list(scoreboard['username']), key=str.casefold):
+#     print("<a href=\"#" + player + "\">" + player + "</a>")
 
 df = getScoreboard().reset_index()
 df_styled = df.style
 df_styled = df_styled.hide_index().hide_columns('index')  # remove index columns
-print(df_styled.hide_index().render())
+# print(df_styled.hide_index().render())
 
 # @ nick a, look at this for styling ideas. Look at maybe text gradient or highlighting based on the score value?
 #  https://pandas.pydata.org/docs/reference/api/pandas.io.formats.style.Styler.html
