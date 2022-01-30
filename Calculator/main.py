4c1be8f472aa3d96bbd3ebc37dc254b6ddcf79db
nickazcarate <nick.azcarate@gmail.com>
2022-01-29 19:36:08 -0800

    cosmetic, remove old code

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,12 +1,6 @@
 from Calculator import *
 from websiteCode import *
-import numpy as np
-import jinja2
-from IPython.display import display
 
-# TODO: sorting ignoring string case: https://stackoverflow.com/questions/10269701/case-insensitive-list-sorting-without-lowercasing-the-result
-
-import pickle
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 calculateScores(1)
@@ -17,42 +11,6 @@ calculateScores(4)
 
 generate_history_html_file()
 generate_scoreboard_html_file()
-print('############################################### HISTORY HTML #############################################################')
-
-for p in range(0, len(player_list)):
-    print("<h2 id=\"" + player_list[p].get_username() + "\" class=\"player-name\">" + player_list[p].get_username() + "</h2>")
-    df = player_list[p].get_history(False).reset_index()
-    df_styled = df.style
-    df_styled = df_styled.hide_index().hide_columns('index')  # remove index columns
-    df_styled = df_styled.hide_columns('question_type')
-    print(df_styled.hide_index().render())
-    print('\n\n')
-
-print('############################################### SCOREBOARD HTML #############################################################')
-
-scoreboard = getScoreboard()
-
-# for player in sorted(list(scoreboard['username']), key=str.casefold):
-#     print("<a href=\"#" + player + "\">" + player + "</a>")
-
-df = getScoreboard().reset_index()
-df_styled = df.style
-df_styled = df_styled.hide_index().hide_columns('index')  # remove index columns
-# print(df_styled.hide_index().render())
 
 # @ nick a, look at this for styling ideas. Look at maybe text gradient or highlighting based on the score value?
 #  https://pandas.pydata.org/docs/reference/api/pandas.io.formats.style.Styler.html
-
-
-
-# np.random.seed(24)
-# df = pd.DataFrame({'A': np.linspace(1, 10, 10)})
-# df = pd.concat([df, pd.DataFrame(np.random.randn(10, 4), columns=list('BCDE'))],
-#                axis=1)
-# df.iloc[0, 2] = np.nan
-#
-# s = df.style
-# print(s.render())
-
-# calculateScores(3)
-# printScores()
